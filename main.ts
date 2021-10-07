723d1b0ea0c0db8af020091074d83691ffb786ba
Jieun35 <90733575+Jieun35@users.noreply.github.com>
2021-10-07 12:23:06 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 let 가변저항 = 0
 basic.forever(function () {
-    basic.showNumber(가변저항)
+    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
 })
 basic.forever(function () {
     가변저항 = pins.analogReadPin(AnalogPin.P0)
     pins.analogWritePin(AnalogPin.P8, pins.analogReadPin(AnalogPin.P0))
+})
+basic.forever(function () {
+    가변저항 = pins.analogReadPin(AnalogPin.P0)
+    if (170 < 가변저항) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+    } else if (170 < 가변저항 && 가변저항 < 340) {
+    	
+    } else if (340 < 가변저항 && 가변저항 < 510) {
+    	
+    } else if (340 < 가변저항 && 가변저항 < 510) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
 })
